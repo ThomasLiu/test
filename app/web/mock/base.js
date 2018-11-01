fca1313688f623323d4e543107e08445239b2c14
Thomas Lau <thomas_0836@qq.com>
2018-11-01 17:40:41 +0800

    add mock

---
+++ app/web/mock/base.js
@@ -0,0 +1,4 @@
+export default {
+  'GET /base/%2Fi18n%2Fzh-CN': {"code":0,"data":{"zh-CN":"中文","en-US":"EN","Change to":"Change to English","Setting":"设置","About Us":"关于我们","Group":"组","test1":"测试1","test2":"测试2","test3":"测试3","test4":"测试4","test5":"测试5"},"msg":"请求成功"},
+  'GET /base/%2Fmenu': {"code":0,"data":{},"msg":"请求成功"}
+}
